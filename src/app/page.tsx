37371c60a726b0a2e6e3ba8fa5e136571c9bd59f
Alejandro Villalobos <is728135@iteso.mx>
2025-01-27 13:43:34 -0600

    Landing - section1 v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ export default function Home() {
       {/* Name and img */}
       <div className="max-w-screen-xl mx-auto
                       grid grid-cols-1 lg:grid-cols-2">
-        <div className="mesh-background p-4">
+        <div className="mesh-background p-4 space-y-4">
           <h1>M. Alejandro Villalobos C.</h1>
           <h2>Computer Systems Engineer</h2>
           <h3>Welcome to my portfolio. Here you&#39;ll find a showcase of my work, projects, and the impact I&#39;ve made in the tech world.</h3>
         </div>
-        <div className="hidden lg:block">
+        <div className="hidden lg:block relative">
           <Image
             src="/images/rubber_ducky.jpg"
             alt="Rubber Ducky Programming"
             width={1024}
-            height={1024}
+            height={768}
           />
         </div>
       </div>
