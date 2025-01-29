898868045d84a33b101169f66badb916c9605313
Alejandro Villalobos <is728135@iteso.mx>
2025-01-28 19:02:01 -0600

    landing - section2 v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,85 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="border-y">
+    <div>
       {/* Name and img */}
-      <div className="max-w-screen-xl mx-auto
-                      grid grid-cols-1 lg:grid-cols-2">
-        <div className="mesh-background p-4 space-y-4">
-          <h1>M. Alejandro Villalobos C.</h1>
-          <h2>Computer Systems Engineer</h2>
-          <h3>Welcome to my portfolio. Here you&#39;ll find a showcase of my work, projects, and the impact I&#39;ve made in the tech world.</h3>
-        </div>
-        <div className="hidden lg:block relative">
-          <Image
-            src="/images/rubber_ducky.jpg"
-            alt="Rubber Ducky Programming"
-            width={1024}
-            height={768}
-          />
+      <div  className="">
+        <div className="max-w-screen-xl mx-auto
+                        grid grid-cols-1 lg:grid-cols-2
+                        border-x border-t">
+          <div className="mesh-background p-4 space-y-6">
+            <h1 className="text-display">M. Alejandro Villalobos C.</h1>
+            <h2 className="text-title-2">Computer Systems Engineer</h2>
+            <h3 className="text-title-3">Welcome to my portfolio. Here you&#39;ll find a showcase of my work, projects, and the impact I&#39;ve made in the tech world.</h3>
+          </div>
+          <div className="hidden lg:block relative">
+            <Image
+              src="/images/rubber_ducky.jpg"
+              alt="Rubber Ducky Programming"
+              width={1024}
+              height={768}
+            />
+          </div>
         </div>
       </div>
 
-      {/* Projects and previews */}
-      <div>
 
+      {/* Projects and previews */}
+      <div className="">
+        <div className="max-w-screen-xl mx-auto
+                        border-x border-t">
+          <div className="grid grid-cols-3 divide-x">
+            <h2 className="text-title-1 p-4">Programming Skills</h2>
+            <p className="text-body p-4 col-span-2">
+              I love creating stuff and problem solving. Programming combines this in the best way. Here are some examples:
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="border-t">
+        <div className="max-w-screen-xl mx-auto
+                        border-x
+                        grid grid-cols-3">
+          <div className="grid grid-cols-1">
+            <a
+              className={`
+              text-title-2
+              hover:text-text-inverse
+              hover:bg-bg-inverse
+              w-auto h-full flex justify-center items-center px-6 py-4
+              duration-200
+            `}>Algorithms</a>
+            <a
+              className={`
+              text-title-2
+              hover:text-text-inverse
+              hover:bg-bg-inverse
+              w-auto h-full flex justify-center items-center px-6 py-4
+              duration-200
+            `}>AI & ML</a>
+            <a
+              className={`
+              text-title-2
+              hover:text-text-inverse
+              hover:bg-bg-inverse
+              w-auto h-full flex justify-center items-center px-6 py-4
+              duration-500
+            `}>About Me</a>
+          </div>
+          <div className="col-span-2">
+            <p>
+              Here goes a preview of the projects I&#39;ve worked on.
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* Contact Info */}
       <div>
 
       </div>
+
+      <div className="border-t"/>
 
     </div>
   );
