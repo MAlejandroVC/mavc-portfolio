5903f66bba693dfda52a7dfd06e7aeeca2cc8c70
Alejandro Villalobos <is728135@iteso.mx>
2025-01-30 19:09:02 -0600

    Minor changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className={`bg-bg-inverse`}>
+    <footer className={`bg-bg-inverse mt-8`}>
       <div className={`max-w-screen-xl mx-auto p-4`}>
         <p className={`text-bg-secondary text-center`}>
           Thanks for stopping by :)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Alejandro Villalobos | Computer Systems Engineer",
   description: "A showcase of my skills and projects as a Computer Systems Engineer.",
 };
 
